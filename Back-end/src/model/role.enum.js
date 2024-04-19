c7929405f7278184909e0d09825ab561e26ae2f1
bisratlike <bisratlike@gmail.com>
2024-04-19 10:15:33 +0300

    last correction

--- Back-end/src/model/role.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Role = void 0;
-var Role;
-(function (Role) {
-    Role["User"] = "user";
-    Role["Admin"] = "admin";
-})(Role = exports.Role || (exports.Role = {}));
